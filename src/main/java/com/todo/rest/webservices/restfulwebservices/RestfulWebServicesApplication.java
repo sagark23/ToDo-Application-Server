61573b063e71d4d1dc2e8a1fcc770b5ef35d4da1
sagark <sagarkhandelwal23@gmai.com>
2021-02-04 13:52:39 +0000

    changing packaging to war

--- src/main/java/com/todo/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/todo/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,9 +2,10 @@ package com.todo.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestfulWebServicesApplication {
+public class RestfulWebServicesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
